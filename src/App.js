a1731d068bb05a93e149463c6cef8dff3359d327
Ekant Chandrakar <ekantchandrakar07@gmail.com>
2023-06-12 17:05:36 +0530

    JSX Component Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const Job = (props) => {
+	return (
+		<div>
+			<h1>{props.name}</h1>
+			<h2>{props.position}</h2>
+			<h2>{props.salary}</h2>
+			<p>{props.company}</p>
+		</div>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Job
+				name="John"
+				position={"Juniour SDE"}
+				salary={900000}
+				company={"Google"}
+			/>
+			<Job
+				name="Kate"
+				position={"SDE - I"}
+				salary={1300000}
+				company={"Amazon"}
+			/>
+			<Job
+				name="Smith"
+				position={"SDE - II"}
+				salary={1500000}
+				company={"Microsoft"}
+			/>
+		</div>
+	);
 }
 
 export default App;
