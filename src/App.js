570059b6ee22bca88f67347f7d29d246399cdf90
Ekant Chandrakar <ekantchandrakar07@gmail.com>
2023-06-14 13:27:41 +0530

    State eg 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
+import { useState } from "react";
 import "./App.css";
-import User from "./User";
 
 function App() {
-	const users = [
-		{ name: "Pedro", age: 21 },
-		{ name: "Jake", age: 25 },
-		{ name: "Jessica", age: 45 },
-	];
+	const [age, setAge] = useState(0);
+
+	const increaseAge = () => {
+		setAge(age + 1);
+	};
 
 	return (
 		<div className="App">
-			{users.map((user, key) => {
-				return <User key={key} name={user.name} age={user.age} />;
-			})}
+			{age}
+			<button onClick={increaseAge}>Increase Age</button>
 		</div>
 	);
 }
